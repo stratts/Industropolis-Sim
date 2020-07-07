5f8e7dddb0164c7a397eb3a51bae8fcf07820749
Stratton Sloane <stratton.s@outlook.com>
2020-07-07 19:40:14 +1000

    Long vehicles can span multiple intersections and paths

--- Vehicles/Vehicle.cs
+++ Vehicles/Vehicle.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Industropolis.Sim
 {
@@ -6,15 +7,20 @@ namespace Industropolis.Sim
     {
         private float _speed = 1; // Tiles per second
         private RouteDirection _direction;
+        private List<VehicleLane> _lanes = new List<VehicleLane>();
+        private List<VehicleNode> _nodes = new List<VehicleNode>();
 
         // Set to null! as they are initialized when GoNext() is called in constructor
         public Route<VehicleNode> Route { get; }
         public VehiclePath CurrentPath { get; private set; } = null!;
         public VehicleLane CurrentLane { get; private set; } = null!;
-        public float PathPos { get; private set; }
+        public float FrontPos { get; private set; }
+        public float RearPos { get; private set; }
         public VehicleNode PrevNode { get; private set; } = null!;
         public VehicleNode NextNode { get; private set; } = null!;
         public VehicleNode Destination { get; private set; } = null!;
+        public float Length { get; set; } = 0.5f;
+        public IReadOnlyList<VehicleLane> Lanes => _lanes;
 
         protected Action? _action;
         protected float _elapsedTime;
@@ -34,33 +40,51 @@ namespace Industropolis.Sim
 
         protected void FollowPath()
         {
-            if (PathPos < CurrentPath.Length - 1) Move();
+            if (FrontPos < CurrentPath.Length - 1) Move();
             else if (CanGoNext())
             {
                 _action = CrossIntersection;
-                NextNode.Occupied = true;
             }
         }
 
         protected void CrossIntersection()
         {
-            if (PathPos < CurrentPath.Length) Move();
+            if (FrontPos < CurrentPath.Length) Move();
             else if (NextNode != Destination)
             {
-                NextNode.Occupied = false;
                 GoNext();
             }
             else
             {
-                NextNode.Occupied = false;
                 DestinationReached();
             }
         }
 
         protected void Move()
         {
-            if (CurrentLane.GetVehicleAhead(this) is Vehicle v && v.PathPos - PathPos < 1) return;
-            PathPos += _speed * _elapsedTime;
+            if (CurrentLane.GetVehicleAhead(this) is Vehicle v && v.RearPos - FrontPos < 0.5f) return;
+            FrontPos += _speed * _elapsedTime;
+            RearPos += _speed * _elapsedTime;
+            if (_lanes.Count > 0)
+            {
+                var rearLane = _lanes[0];
+                if (RearPos > rearLane.Parent.Length)
+                {
+                    RearPos = 0;
+                    rearLane.Depart(this);
+                    _lanes.Remove(rearLane);
+
+                    if (_nodes.Count > 0)
+                    {
+                        var node = _nodes[0];
+                        node.Occupied = false;
+                        _nodes.Remove(node);
+                        Console.WriteLine($"Depart node {node}");
+                    }
+
+                    //Console.WriteLine("Depart lane");
+                }
+            }
         }
 
         protected void FlipDirection()
@@ -71,6 +95,12 @@ namespace Industropolis.Sim
 
         protected void SetDirection(RouteDirection direction)
         {
+            FrontPos = 0;
+            RearPos = -Length;
+            foreach (var lane in _lanes) lane.Depart(this);
+            foreach (var node in _nodes) node.Occupied = false;
+            _nodes.Clear();
+            _lanes.Clear();
             _direction = direction;
 
             switch (direction)
@@ -92,16 +122,23 @@ namespace Industropolis.Sim
 
         protected void GoNext()
         {
-            if (CurrentLane != null) CurrentLane.Depart(this);
+            if (PrevNode != NextNode)
+            {
+                _nodes.Add(NextNode);
+                NextNode.Occupied = true;
+                Console.WriteLine($"Enter node {NextNode}");
+            }
+
             var current = NextNode;
             PrevNode = current;
             NextNode = Route.Next(current, _direction);
             CurrentPath = current.Connections[NextNode];
             CurrentLane = CurrentPath.GetLaneTo(NextNode);
             CurrentLane.Enter(this);
+            _lanes.Add(CurrentLane);
 
-            PathPos = 0;
-            Console.WriteLine($"Start moving to {NextNode.Pos}");
+            FrontPos = 0;
+            //Console.WriteLine($"Start moving to {NextNode.Pos}");
 
             _action = FollowPath;
         }
