77b588d93041818ee00fb10eb240a1e4314b73da
Stratton Sloane <stratton.s@outlook.com>
2020-07-14 22:09:40 +1000

    Improve PathContainer performance

--- Paths/PathContainer.cs
+++ Paths/PathContainer.cs
@@ -22,50 +22,42 @@ namespace Industropolis.Sim
         public event Action<TNode>? NodeAdded;
         public event Action<TPath>? PathAdded;
 
-        private List<TNode> _nodes = new List<TNode>();
+        private Dictionary<IntVector, TNode> _nodes = new Dictionary<IntVector, TNode>();
         private List<TPath> _paths = new List<TPath>();
+        private Dictionary<IntVector, TPath> _pathLocs = new Dictionary<IntVector, TPath>();
 
         public IReadOnlyList<TPath> Paths => _paths;
 
         public TNode? GetNode(IntVector pos)
         {
-            foreach (TNode node in _nodes)
-            {
-                if (node.Pos == pos) return node;
-            }
-
+            if (_nodes.TryGetValue(pos, out var node)) return node;
             return null;
         }
 
         public void AddNode(TNode node)
         {
-            _nodes.Add(node);
+            _nodes[node.Pos] = node;
             NodeAdded?.Invoke(node);
         }
 
         public void RemoveNode(TNode node)
         {
             var _paths = new List<TPath>(node.Connections.Values);
-            foreach (TPath path in _paths)
-            {
-                RemovePath(path);
-            }
-            _nodes.Remove(node);
+            foreach (TPath path in _paths) RemovePath(path);
+            _nodes.Remove(node.Pos);
             node.Remove();
         }
 
         public void AddPath(TPath path)
         {
             this._paths.Add(path);
+            foreach (var pos in path.Source.Pos.GetPointsBetween(path.Dest.Pos)) _pathLocs[pos] = path;
             PathAdded?.Invoke(path);
         }
 
         public TPath? GetPath(IntVector pos)
         {
-            if (GetNode(pos) == null)
-            {
-                foreach (TPath path in GetPaths(pos)) return path;
-            }
+            if (_pathLocs.TryGetValue(pos, out var path) && GetNode(pos) == null) return path;
             return null;
         }
 
@@ -81,6 +73,7 @@ namespace Industropolis.Sim
         {
             path.Source.Disconnect(path.Dest);
             path.Dest.Disconnect(path.Source);
+            foreach (var pos in path.Source.Pos.GetPointsBetween(path.Dest.Pos)) _pathLocs.Remove(pos);
             this._paths.Remove(path);
             path.Remove();
         }
